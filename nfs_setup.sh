e3dc4847c6b2c3476197622b79b97d398bcebc74
tangsongyuan@tieson.cn <tangsongyuan@tieson.cn>
2016-04-15 12:16:43 +0800

    init

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -4,6 +4,7 @@ set -e
 sed -i 's/STATDOPTS=/STATDOPTS="--port 32765 --outgoing-port 32766"/g' /etc/default/nfs-common
 sed -i 's/RPCMOUNTDOPTS="--manage-gids"/RPCMOUNTDOPTS="--manage-gids -p 32767"/g' /etc/default/nfs-kernel-server
 
+
 mounts="${@}"
 
 for mnt in "${mounts[@]}"; do
